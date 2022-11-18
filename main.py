3aef2d89b5fc9d1deb3e8d3589e04f4012cb3a7b
cjc24 <89112537+wolfdogcjc@users.noreply.github.com>
2022-11-18 14:06:30 +0000

    fixed issues

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from tkinter import *
 from tkinter import ttk
 root = Tk()
 frm = ttk.Frame(root, padding=100)
-frm = ttk.color
 frm.grid()
 ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
 ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
